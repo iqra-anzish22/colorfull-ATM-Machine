c06bb9e44cf3184703d5f7346155244152fdeed1
Iqra <iqraanzish48@gmail.com>
2024-06-15 14:58:25 -0700

    Add files via upload
    Adding a Colorfull ATM Machine

---
+++ main.ts
@@ -0,0 +1,82 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from  "chalk";
+
+
+let myBalance = 10000;
+let myPin = 1234;
+
+console.log(chalk.blue("\n \tWelcome to ATM Machine\n"));
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: chalk.yellow( "Enter your pin code:")
+    }
+])
+if (pinAnswer.pin === myPin){  
+    console.log (chalk.green("\npin is Correct, login Successsully!\n"));
+    // console.log(`Curent Acount Balance is ${myBalance}`)
+
+    let operationAns = await inquirer.prompt([
+        {
+             name: "operation",
+            type: "list",
+            message: "Select an operation:",
+            choices:["Withdraw Amount", "Check Balance"]
+        }
+    ])
+
+if (operationAns.operation === "Withdraw Amount"){
+let withdrawAns = await inquirer.prompt([
+    {
+     name: "withdrawMethod",
+     type: "list",
+     message: "select a withdrawl Method:",
+     choices: ["fast Cash","Enter Amount"]
+    }
+])
+if(withdrawAns.withdrawMethod === "fast Cash"){
+    let fastCashAns = await inquirer.prompt([
+        {
+            name: "fastCash",
+            type: "list",
+            message: "Select Amount:",
+            choices: [1000, 2000, 5000,10000, 20000]
+        }
+    ])
+if(fastCashAns.fastCash > myBalance){
+    console.log (chalk.red("Insufficient Balance"));
+}
+ else{
+   myBalance -= fastCashAns.fastCash 
+    console.log(chalk.green(`${fastCashAns.fastCash} withdraw Successfully`));
+    console.log(chalk.green(`your remaining balance is: ${myBalance}`));
+}
+}
+else if(withdrawAns.withdrawMethod === "Enter Amount"){
+    let amountAns = await inquirer.prompt([
+        {
+            name: "amount",
+            type: "number",
+            message: "Enter the amount to withdraw:"
+        }
+    ])
+     if(amountAns.amount > myBalance){
+        console.log(chalk.red("Insufficient Balance"));
+     }
+     else{
+        myBalance -= amountAns.amount;
+        console.log(chalk.green(`${amountAns.amount} withdraw Successfully`));
+        console.log(chalk.green(`your remaining balance is: ${myBalance}`));
+        }
+   }
+}
+else if (operationAns.operation === "Check Balance"){
+      console.log(chalk.yellow(`your Acount Balance is: ${myBalance}`));
+  }
+}
+ else{
+    console.log(chalk.red("pin is Incorrect, Try again!!"));
+ }
